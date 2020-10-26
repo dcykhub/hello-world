6ca6b7f5798d6a3fca2881dc7f78d65f2dbbf5b4
dcykhub <73445679+dcykhub@users.noreply.github.com>
2020-10-26 13:29:16 +0200

    Add files via upload

---
+++ Hello World!.bat
@@ -0,0 +1,31 @@
+@echo off
+title Hi World!
+color 2
+cls
+goto a
+:a
+echo Hi World!
+echo My name is dcyk!
+echo Okay, lemme say you something...
+pause
+goto b
+:b
+cls
+echo It's open-source .BAT file, you can download API on my GitHub.
+echo Soooo.. if you interested in my scripts, you can check what i can do.
+echo Fii-rst. Let me.. check your system.
+echo If you think its a virus, just download source and check. Or use virustotal.
+pause
+goto c
+:c
+cls
+systeminfo
+echo That's your system, omg.
+echo if you interested - my pc specs is I7-2630QM 2.00 GHz, GPU INTEL HD GRAPHICS 3000.
+pause
+goto d
+:d
+msg * I don't know what i forgot to say.
+msg * Soo.. BYE!
+msg * Have a great day!
+exit
